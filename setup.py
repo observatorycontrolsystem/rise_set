da64f193c8a0b481eafb6183fc84fcbd38f86ccd
Jon Nation <jnation@lcogt.net>
2015-04-06 20:07:05 +0000

    updated the setup.py, requirements, and tox file for rise-set to specify build steps

--- setup.py
+++ setup.py
@@ -2,27 +2,11 @@
 
 '''This file controls the packaging and deployment of the rise_set module.
 
-    1) RUN TESTS: Unit tests may be run with
-
-       python setup.py test
-
-    (ignore any logger errors)
-
-    2) CREATE SOURCE: Do
-       python setup.py sdist
-
-       to create a source tarball suitable for distributing.
-
-    3) INSTALL:
-       python setup.py install
-
-       This will download and compile any needed dependencies, then install to
-       your Python package directory.
+    Preferred Installation methods are found in the INSTALLATION file in this directory.
 
 Eric Saunders
 August 2012
 '''
-
 
 from setuptools import setup, find_packages
 
@@ -32,22 +16,22 @@ setup(
     description = 'Routines for accurate rise/set/transit calculations',
     author = 'Eric Saunders',
     author_email = 'esaunders@lcogt.net',
-#    packages = ['rise_set', 'rise_set.test'],
     packages = ['rise_set'],
     dependency_links = [
-       "http://pluto/pyslalib-1.0.tar.gz"
+	"http://buildsba/python/pyslalib"
     ],
     install_requires = [
-      "pyslalib"
+	"mock",
+	"pylint",
+        "pySLALIB"
     ],
-
     package_data = {
         '': ['*.conf'],
     },
 
-
     tests_require = [
-        "nose"
+        "nose",
+	"nosexcover"
     ],
     test_suite = 'nose.collector',
 )
